3575adb58dab1b725a5ab26fa8eba27de25ff88b
Yevhenii Lukashevskyi <aspanio@outlook.com>
2026-05-12 23:57:22 -0500

    feat: split + coloring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::style::{Color, Modifier, Style};
 use ratatui::{DefaultTerminal, Frame};
+use ratatui::{prelude::*, widgets::*};
 use std::process::Command;
-
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     ratatui::run(app)?;
@@ -8,23 +10,56 @@ fn main() -> color_eyre::Result<()> {
 }
 
 fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    let diff = get_git_diff();
+    let diff_chunks = get_diff_chunks(&diff);
     loop {
-        terminal.draw(render)?;
+        terminal.draw(|frame| render_diff(frame, &diff_chunks))?;
         if crossterm::event::read()?.is_key_press() {
             break Ok(());
         }
     }
 }
 
-
-fn render(frame: &mut Frame) {
-    let output = gitDiff();    
-
-    frame.render_widget(output, frame.area());
+fn get_diff_chunks(diff: &str) -> Vec<Line> {
+    diff.split("\n")
+        .map(|str| {
+            if str.starts_with("diff --git") {
+                Line::from(str).style(
+                    Style::default()
+                        .fg(Color::Yellow)
+                        .add_modifier(Modifier::BOLD),
+                )
+            } else if str.starts_with("+") {
+                Line::from(str).style(Style::default().fg(Color::Green))
+            } else if str.starts_with("-") {
+                Line::from(str).style(Style::default().fg(Color::Red))
+            } else if str.starts_with("@@") {
+                Line::from(str).style(Style::default().fg(Color::Cyan))
+            } else {
+                Line::from(str)
+            }
+        })
+        .collect()
 }
 
+fn render_diff(frame: &mut Frame, diff: &[Line]) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([
+            Constraint::Length(3), // фиксированно 3 строки
+            Constraint::Min(0),    // остальное
+        ])
+        .split(frame.area());
 
-fn gitDiff() -> String {
+    let header = Paragraph::new("GitTaTUI").block(Block::default().borders(Borders::ALL));
+    let body = Paragraph::new(diff.to_vec()).block(Block::default().borders(Borders::ALL));
+
+    frame.render_widget(header, chunks[0]);
+    frame.render_widget(body, chunks[1]);
+}
+
+// hello
+fn get_git_diff() -> String {
     let output = if cfg!(target_os = "windows") {
         Command::new("cmd")
             .args(["/C", "git diff"])
